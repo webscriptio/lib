26e1a4c55a361a405a4e184a43ee40edeef8038d
Steve Marx <smarx@smarx.com>
2012-10-27 20:24:19 -0700

    adding sms and call functions

--- twilio.lua
+++ twilio.lua
@@ -10,4 +10,22 @@ local verify = function (request, authToken)
 	return request.headers['X-Twilio-Signature'] == base64.encode(hmac)
 end
 
-return { verify = verify }
+local sms = function (accountsid, authtoken, from, to, body)
+	return http.request {
+		method = 'POST',
+		url = string.format('https://api.twilio.com/2010-04-01/Accounts/%s/SMS/Messages.json', accountsid),
+		data = { From=from, To=to, Body=body },
+		auth = {accountsid, authtoken}
+	}
+end
+
+local call = function (accountsid, authtoken, from, to, url)
+	return http.request {
+		method = 'POST',
+		url = string.format('https://api.twilio.com/2010-04-01/Accounts/%s/Calls.json', accountsid),
+		data={ From=from, To=to, Url=url },
+		auth={accountsid, authtoken}
+	}
+end
+
+return { verify = verify, sms = sms, call = call }
