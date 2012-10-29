1ee25216c83030627a78e7f4c055ea1a9d6841dd
Steve Marx <smarx@smarx.com>
2012-10-29 09:28:39 -0700

    fix verify: underscore.sort returns a table

--- twilio.lua
+++ twilio.lua
@@ -2,7 +2,7 @@ local underscore = require('webscriptio/lib/underscore.lua')
 
 local verify = function (request, authToken)
 	local sts = request.scheme .. '://' .. request.headers.Host .. request.path
-	for k in underscore.sort(underscore.keys(request.form)) do
+	for i,k in ipairs(underscore.sort(underscore.keys(request.form))) do
 		sts = sts .. k .. request.form[k]
 	end
 
