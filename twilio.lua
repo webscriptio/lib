5e2408051c1278a59fd8a1fc4992a83e96d875eb
Steve Marx <smarx@smarx.com>
2012-12-08 10:15:33 -0800

    revise twilio.verify to properly verify GET as well as POST requests

--- twilio.lua
+++ twilio.lua
@@ -1,12 +1,22 @@
 local underscore = require('underscore')
 
-local verify = function (request, authToken)
+-- Verify that an incoming request came from Twilio
+local verify = function (request, authtoken)
+	-- Start with the full URL
 	local sts = request.scheme .. '://' .. request.headers.Host .. request.path
-	for i,k in ipairs(underscore.sort(underscore.keys(request.form))) do
-		sts = sts .. k .. request.form[k]
+	-- Including the querystring (if any)
+	if request.querystring ~= nil then
+		sts = sts .. '?' .. request.querystring
+	end
+	if request.method == 'POST' then
+		-- Add any POST fields
+		local params = request.form or request.query
+		for i,k in ipairs(underscore.sort(underscore.keys(request.form))) do
+			sts = sts .. k .. request.form[k]
+		end
 	end
 
-	hmac = crypto.hmac(authToken, sts, crypto.sha1).digest()
+	hmac = crypto.hmac(authtoken, sts, crypto.sha1).digest()
 	return request.headers['X-Twilio-Signature'] == base64.encode(hmac)
 end
 
